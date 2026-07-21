25dca2f622bd96dbdb0738b74ef06402aea16d8a
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-21 18:42:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,14 +118,15 @@ def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("products", products))
+    app.add_handler(CommandHandler("products", products))
 
-app.add_handler(MessageHandler(filters.Regex("^🛒 Products$"), products))
-app.add_handler(MessageHandler(filters.Regex("^📞 Contact$"), contact))
-app.add_handler(MessageHandler(filters.Regex("^💳 Payment$"), payment))
-app.add_handler(MessageHandler(filters.PHOTO, receive_screenshot))
+    app.add_handler(MessageHandler(filters.Regex("^🛒 Products$"), products))
+    app.add_handler(MessageHandler(filters.Regex("^📞 Contact$"), contact))
+    app.add_handler(MessageHandler(filters.Regex("^💳 Payment$"), payment))
+    app.add_handler(MessageHandler(filters.PHOTO, receive_screenshot))
 
-app.run_polling()
+    app.run_polling()
+
 
 if __name__ == "__main__":
     main()
