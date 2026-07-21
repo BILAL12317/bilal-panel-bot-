a8b0a9309f57985fc5aa0963d0fdc03bcac6a29d
BILAL12317 <mohamedbilal7708962545@gmail.com>
2026-07-21 17:11:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,37 @@
-
 import os
 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
-print("TOKEN:", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot working!")
+    await update.message.reply_text(
+        "👋 Welcome!\n\n"
+        "Use /products to view all products."
+    )
+
+async def products(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = """
+🛒 PRODUCTS
+
+🔹 BR MOD PC VERSION
+• 1 Day - ₹100
+• 10 Days - ₹350
+• 30 Days - ₹650
+
+🔹 DRIP CLIENT PROXY NON ROOT
+• 1 Day - ₹90
+• 3 Days - ₹120
+• 7 Days - ₹230
+• 30 Days - ₹549
+
+🔹 PATO TEAM
+• 3 Days - ₹300
+• 7 Days - ₹450
+• 15 Days - ₹600
+• 30 Days - ₹800
+
 🔹 BR MODS ROOT + NON ROOT
 • 1 Day - ₹90
 • 7 Days - ₹230
@@ -30,7 +53,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 • 7 Days - ₹390
 • 10 Days - ₹490
 • 15 Days - ₹590
-• 30 Days - ₹690🔹 HG CHEAT ROOT + NON ROOT
+• 30 Days - ₹690
+
+🔹 HG CHEAT ROOT + NON ROOT
 • 1 Day - ₹100
 • 7 Days - ₹300
 • 10 Days - ₹400
@@ -59,6 +84,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 • 7 Days - ₹230
 • 15 Days - ₹350
 • 30 Days - ₹550
+"""
+    await update.message.reply_text(text)
+
 def main():
     app = Application.builder().token(TOKEN).build()
 
